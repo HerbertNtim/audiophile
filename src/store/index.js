928b113a8c43c7ac49e94877198afda0484f7735
HerbertNtim <herbertntim15@gmail.com>
2025-06-22 18:11:56 +0000

    local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,27 @@
+// store.js
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cart";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // needed for redux-persist
+    }),
 });
+
+export const persistor = persistStore(store);
